632a991573bfcc237c582db8b99b311bc056341c
Clark Downer <cddowner@gmail.com>
2017-04-18 17:11:02 -0700

    Stash progress

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-console.log('Hello World!');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Knight from './Knight';
+import Square from './Square';
  
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    <App />,
+  	<Square black>
+    	<Knight />
+    </Square>,
     document.getElementById('mount')
   );
 })
